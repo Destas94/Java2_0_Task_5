e5507751581614bbacc2913e50085ab8933a7f0f
Dmitry <dim4n94@yandex.ru>
2020-08-03 18:57:31 +0300

    код полностью переписан

--- src/Task.java
+++ src/Task.java
@@ -1,9 +1,7 @@
+import java.io.IOException;
 public class Task {
-    public static void main(String[] args) {
-        double num1 = Number.getDub();
-        double num2 = Number.getDub();
-        char operation = Operations.getOperation();
-        double result = Calculation.calc(num1,num2,operation);
-        System.out.println("Результат: " + result);
+    public static void main(String[] args) throws IOException {
+        Lines calc = new Lines();
+        calc.readLine();
     }
 }
