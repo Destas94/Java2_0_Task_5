f0c6ab2b12df33143d7f0c4b7136b2cba2feae63
Dmitry <dim4n94@yandex.ru>
2020-08-19 14:47:39 +0300

    add try-catch block

--- src/Lines.java
+++ src/Lines.java
@@ -3,23 +3,43 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Lines extends Calculation {
-    public void readLine() throws IOException
+    public void readLine() //user string reader
     {
         System.out.println("Введите с клавиатуры выражение, которое необходимо вычислить или напишите слово \"exit\" для выхода");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String exprString = null;
-            while(!(exprString = reader.readLine()).equals("exit"))
-            {
-                Calculation.calculate(exprString);
-                if (Calculation.PlusSign == true) {
-                    System.out.println("Результат: "+Calculation.cstring);
-                } else
-                {
-                    Calculation.cstring.insert(0, '-');
-                    System.out.println("Результат: "+Calculation.cstring);
+        try {
+            while (!(exprString = reader.readLine()).equals("exit")) {
+                calculate(exprString);
+                if (PlusSign == true) {
+                    System.out.println("Результат: " + cstring);
+                } else {
+                    cstring.insert(0, '-');
+                    System.out.println("Результат: " + cstring);
                 }
-                Calculation.PlusSign = true;
-                System.out.println("Введите другое выражение или напишите слово \"exit\" для выхода:");
+                PlusSign = true;
+                System.out.println("Введите новое выражение:");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NumberFormatException a) {
+            System.out.println("ERROR!");
+            System.out.println("Введите с клавиатуры выражение, которое необходимо вычислить или напишите слово \"exit\" для выхода");
+            try {
+                while (!(exprString = reader.readLine()).equals("exit")) {
+                    calculate(exprString);
+                    if (PlusSign == true) {
+                        System.out.println("Результат: " + cstring);
+                    } else {
+                        cstring.insert(0, '-');
+                        System.out.println("Результат: " + cstring);
+                    }
+                    PlusSign = true;
+                    System.out.println("Введите другое выражение или напишите слово \"exit\" для выхода:");
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
+    }
+}
